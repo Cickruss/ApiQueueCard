e1db4296e90ab035fd563d94530c4e11233f8923
Cickruss <icaro.o.macedo@gmail.com>
2023-07-17 16:42:30 -0300

    Post do html OK

--- src/index.js
+++ src/index.js
@@ -4,47 +4,58 @@ const express = require("express")
 const app = express()
 const createMessageChannel = require('./messages/messageChannel')
 
-const jsonTest = JSON.stringify({
-    "Agency": "0001",
-    "account": 10101,
-    "cpf" : 12122121,
-    "dateOfBirth": 12112003,
-    "fullName": "icaro macedo",
-    "flag": "Mastercard",
-    "type": "Platinum",
-    "expirationDate": 15
+
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }));
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/front/index.html')
+})
+app.post('/', (req,res) => {
+
+    const card = JSON.stringify({
+        "Agency": req.body.agency,
+        "account": req.body.account,
+        "cpf" : req.body.cpf,
+        "dateOfBirth": req.body.dateOfBirth,
+        "fullName": req.body.fullName,
+        "nameForCard": CreateNameForCard(req.body.fullName),
+        "flag": req.body.flag,
+        "type": req.body.type,
+        "expirationDate": req.body.expirationDate
+    })
+
+    console.log(card);
+    
+    CreateMessage(card)
+    res.send("Formulario enviado!")
 })
 
-const CreateMessage = async (jsonTest) => {
+app.listen(process.env.PORT, () => {
+    console.log('Server on: http://localhost:' + process.env.PORT);
+})
+
+
+
+const CreateMessage = async (card) => {
 
     const messageChannel = await createMessageChannel()
     
 
     if (messageChannel) {
-        messageChannel.sendToQueue(process.env.QUEUE_NAME, Buffer.from(jsonTest))
+        messageChannel.sendToQueue(process.env.QUEUE_NAME, Buffer.from(card))
         console.log("Json send to queue");
     }
 }
 
-CreateMessage(jsonTest)
+function CreateNameForCard(name) {
+    const names = name.trim().split(' ')
+    const firstName = names[0]
+    const lastName = names[names.length - 1]
+    const nameForCard = firstName + ' ' + lastName
+
+    return nameForCard
+  }
 
 
-
-
-
-
-
-
-
-/*
-app.use(express.json())
-
-app.get('/', (req,res) => {
-
-    res.send('Rabbit API')
-})
-
-app.listen(process.env.PORT, () => {
-    console.log('Server on: http://localhost:' + process.env.PORT)
-})
- */
